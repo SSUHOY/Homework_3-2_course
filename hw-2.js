67658a39e17583eee2e49063e53e9278f56428ff
SSUHOY <sammysoul96@gmail.com>
2023-02-23 10:59:22 +0700

    Исправил код программы сезонов года - работает

--- hw-2.js
+++ hw-2.js
@@ -1,13 +1,30 @@
-let monthNumber = 12;
+let monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
     case 1:
+    case 2:
         alert('Зима');
         break;
-    case 2:
-        console.log('Зима');
+    case 3:
+    case 4:
+    case 5:
+        alert('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        alert('Осень');
+        break;
+    case 12:
+        alert('Зима');
         break;
     default:
-        console.log('Ввел что-то не то');
+    case monthNumber > 12:
+        alert('Ввел что-то не то');
         break;
 }
