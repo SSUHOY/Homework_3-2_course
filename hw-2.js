45621437cc8ed4bd75a9665e59d557ee0acbb626
SSUHOY <sammysoul96@gmail.com>
2023-02-24 15:52:03 +0700

    создал проверку на число

--- hw-2.js
+++ hw-2.js
@@ -1,2 +1,8 @@
-let value = prompt('Пожалуйста, введите любое число');
+let value = Number(prompt('Пожалуйста, введите любое число'));
+
+if (isNaN(value)) {
+    alert('Нужно писать число');
+} else {
+    alert('Это число');
+}
 
