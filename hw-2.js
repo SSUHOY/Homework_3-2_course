2bec830a155a605a596b998f2c2b542a5f091f2e
SSUHOY <sammysoul96@gmail.com>
2023-02-25 10:07:16 +0700

    зад. 9, программа определения ос и г.в. смартфона

--- hw-2.js
+++ hw-2.js
@@ -1,8 +1,17 @@
 let attention = alert('У нашего банка есть приложение');
-let clientOS = prompt('Укажите вашу операционную систему');
+let clientOS = prompt('Укажите вашу операционную систему: 1-IOS; 0-ANDROID');
+let askYear = String(prompt('Укажите год выпуска модели телефона'));
+let clientDeviceYear = '2015';
 
-if (clientOS === '1') {
-    console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === '0') {
-    console.log('Установите версию приложения для Android по ссылке');
+
+if (clientOS === '1' && askYear >= clientDeviceYear) {
+    alert('Установите версию приложения для IOS по ссылке')
+} else if (clientOS === '1' && askYear < clientDeviceYear) {
+    alert ('Установите облегченную версию приложения для IOS по ссылке')
+} else if (clientOS === '0' && askYear < clientDeviceYear) {
+    alert('Установите облегченную версию приложения для Android по ссылке')
+} else if (clientOS === '0' && askYear >= clientDeviceYear) {
+    alert('Установите версию приложения для Android по ссылке')
 }
+
+
