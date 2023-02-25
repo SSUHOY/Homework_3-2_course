314cdd57eb63465d9d8fa9e2924e8eb0b69b8667
SSUHOY <sammysoul96@gmail.com>
2023-02-25 09:10:27 +0700

    внес правки в задание 5

--- hw-2.js
+++ hw-2.js
@@ -1,8 +1,27 @@
-let attention = alert('У нашего банка есть приложение');
-let clientOS = prompt('Укажите вашу операционную систему');
-
-if (clientOS === 'ios') {
-    console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === 'android') {
-    console.log('Установите версию приложения для Android по ссылке');
+let monthNumber = Number(prompt('Введите номер месяца'));
+switch (monthNumber) {
+    case 1:
+    case 2:
+    case 12:
+        alert('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        alert('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        alert('Осень');
+        break;
+    default:
+        alert('Ввел что-то не то');
+        break;
 }
+    
