d1e5eb3be8cf3a96a4c1a3ddbe93c82ea5746ac8
SSUHOY <sammysoul96@gmail.com>
2023-02-25 23:14:31 +0300

    создал проверку кода задание 1

--- hw-2.js
+++ hw-2.js
@@ -1,17 +1,9 @@
-let attention = alert('У нашего банка есть приложение');
-let clientOS = prompt('Укажите вашу операционную систему: 1-IOS; 0-ANDROID');
-let askYear = String(prompt('Укажите год выпуска модели телефона'));
-let clientDeviceYear = '2015';
+let password = 'пароль к коду';
+let askuser = prompt('Введите пароль');
 
-
-if (clientOS === '1' && askYear >= clientDeviceYear) {
-    alert('Установите версию приложения для IOS по ссылке')
-} else if (clientOS === '1' && askYear < clientDeviceYear) {
-    alert ('Установите облегченную версию приложения для IOS по ссылке')
-} else if (clientOS === '0' && askYear < clientDeviceYear) {
-    alert('Установите облегченную версию приложения для Android по ссылке')
-} else if (clientOS === '0' && askYear >= clientDeviceYear) {
-    alert('Установите версию приложения для Android по ссылке')
+if (password === askuser) {
+    alert('Пароль введен верно');
+} else if (password !== askuser) {
+    alert('Пароль введен неправильно');
 }
-
 
