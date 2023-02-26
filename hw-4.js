782d9ee33681d44e962f0152b6c9b891ba01dfd0
SSUHOY <sammysoul96@gmail.com>
2023-02-26 10:53:44 +0700

    Задание 4 - вывод зарплаты с помощью for/in

--- hw-4.js
+++ hw-4.js
@@ -13,10 +13,19 @@ do {
 } while (b < 6);
 
 
-
 let c = 7;
 
 do {
     console.log(c);
     c++;
 } while (c < 23);
+
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+}
+
+for (const item in obj) {
+    alert(`${item} - зарплата ${obj[item]} долларов`)
+}
