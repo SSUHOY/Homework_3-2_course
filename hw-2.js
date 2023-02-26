26f14cea97ce812635dc459d08e6f99eb871df51
SSUHOY <sammysoul96@gmail.com>
2023-02-26 09:52:59 +0700

    Задание 3 - создал if/else ветвление

--- hw-2.js
+++ hw-2.js
@@ -13,3 +13,12 @@ if (c > 0 && c < 10) {
 } else {
     console.log('Неверно')
 }
+
+let d = 4;
+let e = 5;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно')
+}
