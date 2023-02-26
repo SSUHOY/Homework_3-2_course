28edd980bf42c2bb072e413a090dd5fe2d81b0cf
SSUHOY <sammysoul96@gmail.com>
2023-02-26 10:07:26 +0700

    Задание 8 - проверка на ОС у пользователя

--- hw-2.js
+++ hw-2.js
@@ -63,3 +63,12 @@ if (isNaN(value)) {
 } else {
     alert('Это число нечетное');
 }
+
+let attention = alert('У нашего банка есть приложение');
+let clientOS = prompt('Укажите вашу операционную систему 1-IOS; 0-Android');
+
+if (clientOS === '1') {
+    console.log('Установите версию приложения для iOS по ссылке');
+} else if (clientOS === '0') {
+    console.log('Установите версию приложения для Android по ссылке');
+}
