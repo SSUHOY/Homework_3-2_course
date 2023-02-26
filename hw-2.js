7c198bc1d6216ad12efd3b8ef13cddf07b033a9e
SSUHOY <sammysoul96@gmail.com>
2023-02-26 10:10:19 +0700

    Задание 9 - создал проверку ОС и г.в смартфона

--- hw-2.js
+++ hw-2.js
@@ -65,10 +65,17 @@ if (isNaN(value)) {
 }
 
 let attention = alert('У нашего банка есть приложение');
-let clientOS = prompt('Укажите вашу операционную систему 1-IOS; 0-Android');
+let clientOS = prompt('Укажите вашу операционную систему: 1-IOS; 0-Android');
+let askYear = String(prompt('Укажите год выпуска модели телефона'));
+let clientDeviceYear = '2015';
 
-if (clientOS === '1') {
-    console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS === '0') {
-    console.log('Установите версию приложения для Android по ссылке');
+if (clientOS === '1' && askYear >= clientDeviceYear) {
+    alert('Установите версию приложения для IOS по ссылке')
+} else if (clientOS === '1' && askYear < clientDeviceYear) {
+    alert ('Установите облегченную версию приложения для IOS по ссылке')
+} else if (clientOS === '0' && askYear < clientDeviceYear) {
+    alert('Установите облегченную версию приложения для Android по ссылке')
+} else if (clientOS === '0' && askYear >= clientDeviceYear) {
+    alert('Установите версию приложения для Android по ссылке')
 }
+
