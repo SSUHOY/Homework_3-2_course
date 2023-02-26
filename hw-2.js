748d51f9f0bfdc841102bef1818b18dbe7117632
SSUHOY <sammysoul96@gmail.com>
2023-02-26 10:02:56 +0700

    Задание 7 - создал проверку на число

--- hw-2.js
+++ hw-2.js
@@ -54,3 +54,10 @@ switch (monthNumber) {
         break;
 }
 
+let value = Number(prompt('Пожалуйста, введите любое число'));
+
+if (isNaN(value)) {
+    alert('Нужно писать число');
+} else {
+    alert('Это число');
+}
