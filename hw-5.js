019350125700dceaaa1f68beda414a3fc377603b
SSUHOY <sammysoul96@gmail.com>
2023-03-01 21:56:03 +0300

    Задание 2:Создал ф-цию с проверкой чет/нечет числа

--- hw-5.js
+++ hw-5.js
@@ -1,11 +1,26 @@
-function comparison(a , b) {
-    if (a > b) {
-        alert(b);
-    } else {
-       alert(a);  
-    }
+// function comparison(a , b) {
+//     if (a > b) {
+//         alert(b);
+//     } else {
+//        alert(a);  
+//     }
+// }
+
+// comparison(8 , 4);
+// comparison(4 , 4);
+// comparison(6 , 6);
+
+
+function value(a) {
+    return a % 2
 }
 
-comparison(8 , 4);
-comparison(4 , 4);
-comparison(6 , 6);
+let result = (Number(value()));
+
+if (result === 0) {
+    alert('Это число четное')
+} else if (result !== 0) {
+    alert('Это число нечетное');
+} 
+
+
