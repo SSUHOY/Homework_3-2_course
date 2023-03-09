51c6dddab7321d8ca21138562d0e98c9e2afaad3
SSUHOY <sammysoul96@gmail.com>
2023-03-09 16:26:40 +0700

    Задание 3 округление по трем подпунктам из ТЗ

--- hw-7.js
+++ hw-7.js
@@ -7,16 +7,23 @@
 
 // Задание 2 - ВОПРОС   как вернуть в 1 элемент массива - в консоли выдает два
 
-function stringGet() {
-    const searchStart = (['Осадки', 'Температура', 'Освещенность', 'Давление']);
-    const search = 'ос';
+// function stringGet() {
+//     const searchStart = (['Осадки', 'Температура', 'Освещенность', 'Давление']);
+//     const search = 'ос';
    
-    searchStart.forEach((searchStart) => {
-        if (searchStart.toLowerCase().startsWith(search.toLowerCase()))  {
-            console.log(new Array(searchStart));
-        }
-});
-}
+//     searchStart.forEach((searchStart) => {
+//         if (searchStart.toLowerCase().startsWith(search.toLowerCase()))  {
+//             console.log(new Array(searchStart));
+//         }
+// });
+// }
 
-console.log(stringGet());
+// console.log(stringGet());
 
+// Задание 3 
+
+let number = 32.58884;
+
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
