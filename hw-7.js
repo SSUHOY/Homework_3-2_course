e195dc6ab3c98fe137e243fe09f12a8786614bf8
SSUHOY <sammysoul96@gmail.com>
2023-03-09 16:11:18 +0700

    Задание 2 функция с поиском startWith

--- hw-7.js
+++ hw-7.js
@@ -1,6 +1,22 @@
 // Задание 1
 
-const stringLowerCase = 'js';
-const stringToUpperCase = stringLowerCase.toUpperCase();
+// const stringLowerCase = 'js';
+// const stringToUpperCase = stringLowerCase.toUpperCase();
 
-console.log(stringToUpperCase);
+// console.log(stringToUpperCase);
+
+// Задание 2 - ВОПРОС   как вернуть в 1 элемент массива - в консоли выдает два
+
+function stringGet() {
+    const searchStart = (['Осадки', 'Температура', 'Освещенность', 'Давление']);
+    const search = 'ос';
+   
+    searchStart.forEach((searchStart) => {
+        if (searchStart.toLowerCase().startsWith(search.toLowerCase()))  {
+            console.log(new Array(searchStart));
+        }
+});
+}
+
+console.log(stringGet());
+
