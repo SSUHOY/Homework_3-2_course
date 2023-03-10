fa8a37b6d1dc317e8e201b0f69ec3d0b54f6d501
SSUHOY <sammysoul96@gmail.com>
2023-03-10 09:51:32 +0700

    Задание 10 вывод даты в установленной форме

--- hw-7.js
+++ hw-7.js
@@ -78,7 +78,19 @@
 
 // Задание 9 
 
-let currentDate = new Date();
-currentDate.setDate(currentDate.getDate() + 73);
+// let currentDate = new Date();
+// currentDate.setDate(currentDate.getDate() + 73);
 
-console.log(currentDate);
+// console.log(currentDate);
+
+// Задание 10 
+
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+let myDate = new Date();
+
+let fullDate = 'Сегодня: ' + myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() + ", " + days[myDate.getDay()];
+
+console.log(fullDate);
