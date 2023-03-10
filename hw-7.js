f0fff35f5ff6565c6af162c1caea2a37dc42a667
SSUHOY <sammysoul96@gmail.com>
2023-03-10 16:34:38 +0700

    Задание 11 привязал игру к html

--- hw-7.js
+++ hw-7.js
@@ -136,6 +136,24 @@ function askUserGame() {
         alert('Неверно!');
     }
 };
-askUserGame();
+
+function monthTake() {
+
+    let a = Number(prompt('Введите номер месяца, а программа определит к какому времени года он относится'));
+  
+    if (a === 1 || a === 2 || a === 12) {
+      return console.log('Это зима!')
+    } else if (a === 3 || a === 4 || a === 5) {
+      return console.log('Это весна!')
+    } else if (a === 6 || a === 7 || a === 8) {
+      return console.log('Это лето!')
+    } else if (a === 9 || a === 10 || a === 11) {
+      return console.log('Это осень!')
+    } else {
+      console.log('Увы, такого месяца нет');
+    }
+  
+  }
+  
 
 
