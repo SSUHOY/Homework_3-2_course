d0906a4880a8a44c2e2dc0adf2cef38ae6c8c491
SSUHOY <sammysoul96@gmail.com>
2023-03-13 19:34:12 +0700

    Задание 2 обьекты массива по возрастанию

--- hw-8.js
+++ hw-8.js
@@ -1,30 +1,65 @@
 // Задание 1
 
-let arrNum = [3, 6, 7, 9];
+// let arrNum = [3, 6, 7, 9];
 
-function getSumNumber() {
-  let getResult = arrNum.reduce((acc, num) => acc + num, 0);
-  console.log(getResult);
+// function getSumNumber() {
+//   let getResult = arrNum.reduce((acc, num) => acc + num, 0);
+//   console.log(getResult);
+// }
+
+// function getMultNumber() {
+//   let getResult = Math.round(arrNum.reduce((acc, num) => acc * num));
+//   console.log(getResult);
+// }
+
+// function getDivNumber() {
+//   let getResult = Math.round(arrNum.reduce((acc, num) => (acc / num)*10));
+//   console.log(getResult);
+// }
+
+// function printFrame(callback) {
+//   console.log(arrNum);
+//   callback();
+// }
+
+// printFrame(getSumNumber);
+// printFrame(getMultNumber);
+// printFrame(getDivNumber);
+
+// Задание 2 
+
+const users = [
+  { name: 'Jon', age: 22 },
+  { name: 'Richard', age: 18 },
+  { name: 'Anton', age: 32 },
+  { name: 'Lida', age: 23 },
+  { name: 'Bob', age: 44 }
+];
+
+
+function compareUser(a, b) {
+  if (a.age > b.age) return 1;
+  if (a.age < b.age) return -1;
+  return 0;
 }
 
-function getMultNumber() {
-  let getResult = Math.round(arrNum.reduce((acc, num) => acc * num));
-  console.log(getResult);
+function sort(arr, compareFunction) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      const condition = compareFunction
+        ? compareFunction(arr[i], arr[j]) > 0 
+        : arr[i].toString() > arr[j].toString();
+      if (condition) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
-function getDivNumber() {
-  let getResult = Math.round(arrNum.reduce((acc, num) => (acc / num)*10));
-  console.log(getResult);
-}
-
-function printFrame(callback) {
-  console.log(arrNum);
-  callback();
-}
+users.sort(compareUser);
+console.log(users);
 
 
-
-
-printFrame(getSumNumber);
-printFrame(getMultNumber);
-printFrame(getDivNumber);
