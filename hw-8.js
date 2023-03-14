c225bdb957b776950c82e0d26fde5621a0de578d
SSUHOY <sammysoul96@gmail.com>
2023-03-14 10:57:25 +0700

    Задание 3 манипуляции над массивами

--- hw-8.js
+++ hw-8.js
@@ -28,38 +28,59 @@
 
 // Задание 2 
 
-const users = [
-  { name: 'Jon', age: 22 },
-  { name: 'Richard', age: 18 },
-  { name: 'Anton', age: 32 },
-  { name: 'Lida', age: 23 },
-  { name: 'Bob', age: 44 }
-];
+// const users = [
+//   { name: 'Jon', age: 22 },
+//   { name: 'Richard', age: 18 },
+//   { name: 'Anton', age: 32 },
+//   { name: 'Lida', age: 23 },
+//   { name: 'Bob', age: 44 }
+// ];
 
 
-function compareUser(a, b) {
-  if (a.age > b.age) return 1;
-  if (a.age < b.age) return -1;
-  return 0;
+// function compareUser(a, b) {
+//   if (a.age > b.age) return 1;
+//   if (a.age < b.age) return -1;
+//   return 0;
+// }
+
+// function sort(arr, compareFunction) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i; j < arr.length; j++) {
+//       const condition = compareFunction
+//         ? compareFunction(arr[i], arr[j]) > 0 
+//         : arr[i].toString() > arr[j].toString();
+//       if (condition) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// users.sort(compareUser);
+// console.log(users);
+
+// Задание 3 
+
+let arr = [1, '4', 9, 'two'];
+let arrSecond = [1, '4', false, 9, 'two'];
+
+function each(callback) {
+    callback();
 }
 
-function sort(arr, compareFunction) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      const condition = compareFunction
-        ? compareFunction(arr[i], arr[j]) > 0 
-        : arr[i].toString() > arr[j].toString();
-      if (condition) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-  return arr;
+function reversedArrFunc() {
+    let reverseArr = arr.reverse();
+    console.log(arr);
 }
 
-users.sort(compareUser);
-console.log(users);
+function toNumberArrFunc() {
+    let toNumberArr = arrSecond.map((el) => +el).filter(item => item !== isNaN);
+    let toNumberArrPop = toNumberArr.pop();
+    console.log(toNumberArr);
+}
 
-
+each(reversedArrFunc);
+each(toNumberArrFunc);
